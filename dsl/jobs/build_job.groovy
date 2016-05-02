33e8c3d646dcf29695a7394643e4b67d94d5b05f
robsonbittencourt <robson.luizv@gmail.com>
2016-05-02 20:36:02 -0300

    Criando primeiros métodos do JobHelper e um arquivo para criação de um job de build

--- dsl/jobs/build_job.groovy
+++ dsl/jobs/build_job.groovy
@@ -5,4 +5,6 @@ import javaposse.jobdsl.dsl.Job
 
 import com.jenkins.job.dsl.lab.job.JobHelper
 
-Job jenkins_job_dsl_lab_build =  JobHelper.createJob(this as DslFactory, "jenkins-job-dsl-lab build")
+Job job_dsl_lab_build = JobHelper.createJob(this as DslFactory, "jenkins-job-dsl-lab build")
+JobHelper.addGitRepo(job_dsl_lab_build, "https://github.com/robsonbittencourt/jenkins-job-dsl-lab", "master")
+JobHelper.addGradleStep(job_dsl_lab_build, "clean test")
--- src/main/groovy/com/jenkins/job/dsl/lab/job/JobHelper.groovy
+++ src/main/groovy/com/jenkins/job/dsl/lab/job/JobHelper.groovy
@@ -9,4 +9,26 @@ class JobHelper {
 		def Job job = dslFactory.job(name)
 		job
 	}
+
+	public static Job addGitRepo(Job job, String repo, String repoBranch) {
+		job.scm {
+			git {
+				remote {
+					name('origin')
+					url(repo)
+				}
+				branch(repoBranch)
+			}
+		}
+		job
+	}
+
+	public static Job addGradleStep(Job job, String step) {
+		job() {
+			steps {
+				gradle { tasks(step) }
+			}
+		}
+		job
+	}
 }
