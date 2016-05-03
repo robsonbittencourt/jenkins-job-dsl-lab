e216e972be78674873dac8ea9d6425c2a5bae563
robsonbittencourt <robson.luizv@gmail.com>
2016-05-02 23:10:41 -0300

    Adicionando script para criar pipeline

---
+++ dsl/pipeline/pipeline.groovy
@@ -0,0 +1,12 @@
+import javaposse.jobdsl.dsl.views.DeliveryPipelineView.Sorting
+
+deliveryPipelineView('Dummy Pipeline Example') {
+	pipelineInstances(5)
+	columns(1)
+	sorting(Sorting.LAST_ACTIVITY)
+	updateInterval(10)
+	enableManualTriggers(true)
+	showAvatars(true)
+	showChangeLog(true)
+	pipelines { component('Dummy Pipeline Example', 'jenkins-job-dsl-lab build') }
+}
